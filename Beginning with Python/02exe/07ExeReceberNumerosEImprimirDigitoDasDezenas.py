9d61cec03f4eb2937009550df9a01f7315fda675
Cleiton Dias <s.diascleiton@gmail.com>
2020-03-03 18:19:20 -0300

    E07 | Imprimir digito das dezenas e centenas de numeros lidos

---
+++ Beginning with Python/02exe/07ExeReceberNumerosEImprimirDigitoDasDezenas.py
@@ -0,0 +1,12 @@
+# Desenvolver um algoritmo que recebe um número inteiro de 0 a 99 e
+# imprime o dígito das dezenas e o dígito das unidades desse número.
+#
+# Divisão inteira do número 10 - não colocar virgula nem acrescentar
+# 0 na divisão.
+
+
+numero = int(input("Digite um número (0-99):"))
+
+divisao = numero / 10
+
+print(divisao)
