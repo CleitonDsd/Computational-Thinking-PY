02cd0ff83ef413750b57c2e2fa1d01163539b931
Cleiton Dias <s.diascleiton@gmail.com>
2020-03-18 22:21:29 -0300

    Exe01 | Exibindo número maior que 10 - desvio de fluxo

---
+++ Beginning with Python/03exe/01Exe_ExibirSeNMAiorQue10.py
@@ -0,0 +1,14 @@
+# Faça um algoritmo que receba um número e mostre uma mensagem caso este número seja
+# maior que 10.
+
+numero = float (input("Digite um número: "))
+
+if numero > 10:
+    print(numero , " é maior que 10")
+elif numero == 10:
+    print(numero , " é igual a 10")
+else:
+    print(numero , " é menor que 10")
+
+
+
