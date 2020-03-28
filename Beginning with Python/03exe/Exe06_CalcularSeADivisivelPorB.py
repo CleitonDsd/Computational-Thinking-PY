93c5cd9f41d15a2ba35b4383a3818eaded2e8f31
Cleiton Dias <s.diascleiton@gmail.com>
2020-03-27 21:18:41 -0300

    E6 | Calcular se A é divisivel por B

--- Beginning with Python/03exe/Exe06_CalcularSeADivisivelPorB.py
+++ Beginning with Python/03exe/Exe06_CalcularSeADivisivelPorB.py
@@ -1,1 +1,10 @@
 # Faça um programa para ler dois números inteiros A e B e informar se A é divisível por B.
+
+valorA = int(input("Valor A: "))
+valorB = int(input("Valor B: "))
+
+
+if (valorA % valorB == 0):
+    print(valorA, " É DIVISIVEL por", valorB)
+else:
+    print(valorA, " não é divisivel por", valorB)
