397c667c3f2234ede41a45d88ece94559223dc02
Cleiton Dias <s.diascleiton@gmail.com>
2020-04-21 19:42:45 -0300

    E13 - (not started) Numeros palindromos

---
+++ Beginning with Python/04Exe/13ExeNumerosPalindromos.py
@@ -0,0 +1,6 @@
+# Dizemos que um número natural n é palíndromo se o 1o algarismo de n é igual ao seu último
+# algarismo, o 2o algarismo de n é igual ao penúltimo algarismo, e assim sucessivamente.
+# Exemplos: 567765 e 32423 são palíndromos. 567675 não é palíndromo.
+
+numero = int(input("Digite um número: "))
+contador = 0
