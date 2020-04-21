5872831e00d5de58a3c416728701f74ab9208269
Cleiton Dias <s.diascleiton@gmail.com>
2020-04-21 19:40:30 -0300

    E4 Quantidade de positivos e negativos

---
+++ Beginning with Python/04Exe/04ExeContarQuantidadePositivosENegativos.py
@@ -0,0 +1,19 @@
+# Dados n um inteiro positivo e uma sequência de n números reais, escreva um algoritmo que
+# conta e imprime a quantidade de números positivos e a quantidade de números negativos.
+
+numerosPositivos = 0
+somaPositivos = 0
+numerosNegativos = 0
+somaNegativos = 0
+
+quantidadeNumeros = int(input("Quantidade de números na sequência: "))
+
+while():
+
+    if (numero < 0):
+        numerosNegativos = numerosNegativos + 1
+        somaNegativos = somaNegativos + numerosNegativos
+
+    elif(numero > 1):
+        numerosPositivos = numerosPositivos + 1
+        somaPositivos = somaPositivos + numerosPositivos
