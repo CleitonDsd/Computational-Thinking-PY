b4bc0aabde86fe4a7528dd55500af1b677b8e2f9
Cleiton Dias <s.diascleiton@gmail.com>
2020-05-16 00:05:26 -0300

    alteração dos diretorios de aula

---
+++ Beginning with Python/lista-extra/SegmentoCrescenteMaximo.py
@@ -0,0 +1,19 @@
+
+
+atual = 3
+if ant < atual:
+    comp = comp + 1
+ant = atual
+
+atual = 4
+if ant < atual:
+    comp = comp + 1
+ant = atual
+
+atual = 1
+if ant < atual:
+    comp = comp + 1
+
+else:
+    comp = 1
+ant = atual
