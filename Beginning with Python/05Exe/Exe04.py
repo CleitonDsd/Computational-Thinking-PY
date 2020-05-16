5205f44f4e74bda40d49da12f73e495f89203e18
Cleiton Dias <s.diascleiton@gmail.com>
2020-05-16 00:02:53 -0300

    Função para saber permutação de números

--- Beginning with Python/05Exe/Exe04.py
+++ Beginning with Python/05Exe/Exe04.py
@@ -1,16 +1,35 @@
-def contadigito (n, d):
 
+
+def contadigito(n, d):
     contador = 0
-
     while n != 0:
-        dig = n % 0
+        dig = n % 10
         if dig == d:
-           contador += 1
+            contador = contador + 1
+        n = n // 10
 
-        n = n / 10
+    return contador
 
 
-num = 1259035
-d = 5
-qtd = contadigito(num, d)
-print(qtd)
+#Resolução do item b
+a = int(input("Informe a: "))
+b = int(input("Informe b: "))
+digi = 1
+qtd_a = 0
+qtd_b = 0
+
+while qtd_a == qtd_b and digi <= 9:
+    qtd_a = contadigito(a, digi)
+    qtd_b = contadigito(b, digi)
+    digi = digi + 1
+
+if qtd_a != qtd_b:
+    print("Não formam uma permutação!")
+else:
+    print("Eles formam uma permutação")
+
+
+
+
+
+
