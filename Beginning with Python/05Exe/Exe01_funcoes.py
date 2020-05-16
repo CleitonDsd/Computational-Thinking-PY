be7faa31b41f77209d48eb411591dc12ec3f6c77
Cleiton Dias <s.diascleiton@gmail.com>
2020-05-15 23:59:50 -0300

    Função para retornar número primo

--- Beginning with Python/05Exe/Exe01_funcoes.py
+++ Beginning with Python/05Exe/Exe01_funcoes.py
@@ -1,13 +1,26 @@
-# números primos
 
-def ePrimo (num):
-
-    if num == 1: #não é primo[
+def ehPrimo(num):
+    if num == 1: #não é primo
         return False
-
     else:
         div = 2
-        while num % 2 != 0:
+        while num % div != 0:
             div = div + 1
 
-    
+        if num == div:
+            return True
+        else:
+            return False
+
+
+def ehPerfeito(num):
+    soma = 0
+    div = 1
+    while num > div:
+        if num % div == 0:
+            soma = soma + div
+        div = div + 1
+    if soma == num:
+        return True
+    else:
+        return False    
